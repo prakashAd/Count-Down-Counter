3430a0c40fb19dc753175e87718fbd99d576240a
prakashAd <adhikariprakash992@gmail.com>
2023-01-17 12:20:27 +0545

    Count

---
+++ app.js
@@ -0,0 +1,25 @@
+
+     var days =document.getElementById('days')   
+     var hours =document.getElementById('hours')   
+     varminutes =document.getElementById('minutes')   
+     var seconds =document.getElementById('seconds')   
+
+     const currentYear = new Date().getFullYear()
+     const currentYearTime = new Date("Jan 01 ${currentYear + 1} 00:00:00")
+
+     function updateCountdown(){
+        const currentTime = new Date()
+        const diff = newYearTime - currentTime
+
+        const d =Math.floor(diff / 1000 / 60 /60 /24)
+        const h =Math.floor(diff / 1000 / 60 /60) % 24
+        const m =Math.floor(diff / 1000 / 60) %60
+        const s =Math.floor(diff / 1000 )%60
+     
+        
+            days.innerHTML = d;
+            hours.innerHTML = h < 10 ?'0'+ h: h;
+            minutes.innerHTML = m < 10 ?'0'+ h: h;
+            seconds.innerHTML = s < 10 ?'0'+ s: s;
+        }
+        setInterval(updateCountdown,1000)
